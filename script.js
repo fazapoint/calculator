15c632f3da866d9c4fd14861059c19995c572abd
Faza Ramadhani <53871962+fazapoint@users.noreply.github.com>
2023-11-08 18:28:17 +0700

    finished design ui
    adding mousedown and mouseup when button pressed, and change color when hovering it

--- script.js
+++ script.js
@@ -1,1 +1,16 @@
-console.log('hello')
+const buttons = document.querySelectorAll('.btn');
+
+function btnDown(event) {
+    const button = event.target;
+    button.style.transform = 'translateY(2px)';
+}
+
+function btnUp(event) {
+    const button = event.target;
+    button.style.transform = 'translateY(0px)';
+}
+
+buttons.forEach((button) => {
+    button.addEventListener('mousedown', btnDown);
+    button.addEventListener('mouseup', btnUp)
+})
