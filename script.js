c0a5af709b35bacafccafb997303727b8d7c5c28
Faza Ramadhani <53871962+fazapoint@users.noreply.github.com>
2024-02-23 18:00:19 +0700

    Add button animation
    When button pressed it will animate like it pressed

--- script.js
+++ script.js
@@ -116,9 +116,22 @@ deleteButton.addEventListener('click', () => {
 });
 
 
+////// animation
 
+function btnDown(event){
+    const button = event.target;
+    button.style.transform = 'translateY(2px)';
+}
 
+function btnUp(event){
+    const button = event.target;
+    button.style.transform = 'translateY(0px)';
+}
 
+allButtons.forEach((button) => {
+    button.addEventListener('mousedown', btnDown);
+    button.addEventListener('mouseup', btnUp)
+});
 
 
 
